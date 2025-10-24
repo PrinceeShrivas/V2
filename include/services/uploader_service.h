5e4469e29e13d7198a917fed00d408a69a230167
PrinceeShrivas <princeshrivas2502@gmail.com>
2025-10-24 09:28:54 +0530

    Update uploader_service.h

--- include/services/uploader_service.h
+++ include/services/uploader_service.h
@@ -1,13 +1,10 @@
 #ifndef UPLOADER_SERVICE_H
 #define UPLOADER_SERVICE_H
-
 #include <string>
 using namespace std;
-
 void uploaderAddScholarship(const string &uploaderID);
 void uploaderUpdateScholarship(const string &uploaderID);
 void uploaderDeleteScholarship(const string &uploaderID);
 void uploaderShowAll(const string &uploaderID);
 void uploaderMenuLoop(const string &uploaderID);
-
 #endif
